0d4d1c90c736f84542d8ff0eab20730b2098583c
Andrea Schiavini <metalelf0@gmail.com>
2021-05-13 09:37:47 +0200

    ✨ Upstream theme changes + fix script

--- utils/theme-extractor.sh
+++ utils/theme-extractor.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 # Script used to extract themes from the original theme.sh repo
 #   and separate them into individual files
-mkdir themes
+mkdir -p themes
 while read line
 do 
   if ! [[ "$line" =~ .*:.* ]] then 
     if ! [[ -z "$line" ]] then 
       theme="$line"
+      echo -n > themes/$theme
     fi
   else
     echo "$line" >> themes/$theme
